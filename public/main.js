4a3e82e495ec82f15c5fd0cbe1dc16cb8a93ecbe
Victoria Tiller <vijotiller@gmail.com>
2021-05-17 16:04:07 -0400

    got username

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  let numberOfCupsOfCoffee = 2
+  let fullName = `Victoria Tiller`
+  console.log(numberOfCupsOfCoffee + ` ` + fullName)
+
+  const aboutMe = {
+    fullName: 'Victoria Tiller',
+    luckyNumber: 14,
+    favoriteMovies: Array('The Road to El Dorado', 'Shrek', 'A Goofy Movie'),
   }
+
+  let userName = window.prompt('What is your name? ')
+  console.log('Sup, ' + userName)
 }
 
 document.addEventListener('DOMContentLoaded', main)
